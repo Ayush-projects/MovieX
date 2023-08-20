5f00d8908d42d51024ca09cd89ca023a50bdd4a0
Ayush-projects <kayush479@gmail.com>
2023-08-20 10:03:04 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
+const PORT = process.env.PORT || 3000
+
+
+app.set("view engine", "ejs")
+
+ 
+
+
+
+
+
+
+
+app.listen(PORT, (err)=>{
+    if(err)
+    console.log(err)
+    else
+    console.log("Server is running on PORT:", PORT)
+})
+
+
+
+
